ed3847e3fd60d6247f5e19305515150b6eb607a2
Erik Karlsson <erik.karlsson@keg.com>
2024-03-08 00:52:51 +0000

    Added api load request

--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,53 @@
 "use client";
 
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
 import { Helmet } from "react-helmet";
-import { useEffect, useState } from 'react';
-import { useSearchParams } from 'next/navigation'
+import { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
 
 export default function Home() {
   const [robots, setRobots] = useState<string>("index,follow");
   const [header, setHeader] = useState<string>("Index Me");
 
-  
+  const [loadingMessage, setLoadingMessage] = useState<string>("Loading Data");
+
   const searchParams = useSearchParams();
 
-  const waitTime = Number.parseInt(searchParams.get('wait') || '2500');
+  const waitTime = Number.parseInt(searchParams.get("wait") || "2500");
+  const loadTime = Number.parseInt(searchParams.get("load") || "2");
 
   useEffect(() => {
     setTimeout(() => {
       setRobots("noindex, nofollow");
       setHeader("Don't Index Me");
     }, waitTime);
-  }, [waitTime]);
+
+    fetch(`https://httpbin.org/delay/${loadTime}`)
+      .then((data) => {
+        setLoadingMessage("Data is now loaded");
+      })
+      .catch((err) => {
+        setLoadingMessage("Error loading data!");
+        console.error(err);
+      });
+  }, [waitTime, loadTime]);
 
   return (
     <main className={styles.main}>
+      <Helmet>
+        <meta name="robots" content={robots} />
+      </Helmet>
       <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
           <code className={styles.code}>app/page.tsx</code>
         </p>
       </div>
-      
+
       <h1>{header}</h1>
-      <Helmet>
-        <meta name="robots" content={robots} />
-      </Helmet>
+
+      <p>{loadingMessage}</p>
 
       <div className={styles.center}>
         <Image
@@ -101,11 +114,9 @@ export default function Home() {
           <h2>
             Data <span>-&gt;</span>
           </h2>
-          <p>
-            Fully-typed real-time API with NoSQL database.
-          </p>
+          <p>Fully-typed real-time API with NoSQL database.</p>
         </a>
       </div>
     </main>
-  )
+  );
 }
