12b307006f45ee0ce4a7f6fef8350271b03402fc
Erik Karlsson <erik.karlsson@keg.com>
2024-03-11 17:16:12 +0100

    Added custom welcome email Added isdone prop to todo

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -7,7 +7,9 @@ import { defineAuth } from '@aws-amplify/backend';
  */
 export const auth = defineAuth({
   loginWith: {
-    email: true,
+    email: {
+      verificationEmailSubject: "Welcome, Please do verify your email to login!"
+    },
     // add social providers
     // externalProviders: {
     /**
