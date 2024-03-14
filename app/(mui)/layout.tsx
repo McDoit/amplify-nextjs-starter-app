6de4736831e651e292681511516f4ed271379757
Erik Karlsson <erik.karlsson@keg.com>
2024-03-14 23:47:20 +0000

    crossOrigin fix

--- app/(mui)/layout.tsx
+++ app/(mui)/layout.tsx
@@ -53,7 +53,7 @@ export default function RootLayout({
           name="viewport"
           content="width=device-width, initial-scale=1, user-scalable=0, maximum-scale=1, minimum-scale=1"
         />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link
       href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,800;1,300;1,400;1,600;1,800&family=Raleway:wght@700;800&display=swap"
