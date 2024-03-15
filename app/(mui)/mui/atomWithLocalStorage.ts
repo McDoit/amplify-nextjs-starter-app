2d840aed278aac7e350c7e96d8c70b071eeaaa1f
Erik Karlsson <erik.karlsson@keg.com>
2024-03-15 11:49:16 +0000

    using base atom for now

--- app/(mui)/mui/atomWithLocalStorage.ts
+++ app/(mui)/mui/atomWithLocalStorage.ts
@@ -3,22 +3,24 @@ import { atom, Atom, PrimitiveAtom} from "jotai";
 export function atomWithLocalStorage<T>(key: string, initialValue: T): PrimitiveAtom<T> {
   function getInitialValue() {
     // if (typeof window !== 'undefined') {
-    const item = localStorage?.getItem(key);
-    if (item !== null) {
-      return JSON.parse(item) as T;
-    }
-  //}
+  //   const item = localStorage?.getItem(key);
+  //   if (item !== null) {
+  //     return JSON.parse(item) as T;
+  //   }
+  // //}
     return initialValue as T;
   }
   const baseAtom = atom(getInitialValue());
-  const derivedAtom = atom(
-    (get) => get(baseAtom),
-    (get, set, update) => {
-      const nextValue =
-        typeof update === "function" ? update(get(baseAtom)) : update;
-      set(baseAtom, nextValue);
-      localStorage.setItem(key, JSON.stringify(nextValue));
-    }
-  );
-  return derivedAtom;
+
+  return baseAtom;
+  // const derivedAtom = atom(
+  //   (get) => get(baseAtom),
+  //   (get, set, update) => {
+  //     const nextValue =
+  //       typeof update === "function" ? update(get(baseAtom)) : update;
+  //     set(baseAtom, nextValue);
+  //     localStorage.setItem(key, JSON.stringify(nextValue));
+  //   }
+  // );
+  // return derivedAtom;
 }
